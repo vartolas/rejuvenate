53583d4f8e467eb035a8c0035fc55ae9045e1a29
Rune Myrskog <rune.myrskog@mail.utoronto.ca>
2020-10-23 11:11:42 -0400

    set up react app folder structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="title">This will be an app one day</h1>
+    </div>
+  );
+}
+
+export default App;
