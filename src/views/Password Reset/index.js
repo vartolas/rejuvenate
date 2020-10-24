22b503ab950ec7062e98e7ad115ed49307296bc2
Danny <danisnotonfire.chen@mail.utoronto.ca>
2020-10-24 18:10:52 -0400

    I added password reset fields and buttons that lead to the home page and back to the login page.
    - TODO: I should add a credentials checker to verify that the username already exists, and the new password is strong enough (e.g. at least 1 alphanumeric character, at least 1 symbol, etc.)
    - TODO: I should think about making a component called PasswordResetForm to make my password reset page more modular.

--- src/views/Password Reset/index.js
+++ src/views/Password Reset/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import './styles.css';
-import SpanLink from '../../react-components/SpanLink';
 
 export default class PasswordReset extends React.Component {
-    render(){
+    render() {
         return (
             <div id="passwordResetContainer">
                 <h1>This is the password reset page</h1>
-                <SpanLink to="/home" name="reset password"/>
+                <br></br>
+
+                {/* A password reset form */}
+                <div>
+                    <label>
+                        Existing Username: <input type="text" name="username" />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        New Password: <input type="text" name="password" />
+                    </label>
+                </div>
+
+                <form action="/">
+                    <br></br>
+                    <input type="submit" value="Go Back" />
+                </form>
+                <form action="/home">
+                    <br></br>
+                    <input type="submit" value="Reset Password" />
+                </form>
             </div>    
         );
     }
