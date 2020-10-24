244f3bf597d3336b5c6c03c8e92a12853c19fa33
Danny <danisnotonfire.chen@mail.utoronto.ca>
2020-10-24 18:01:08 -0400

    Login page: I added login fields and links to the Register and Password Reset pages.
    - TODO: I should add a credentials checker to verify that user credentials are correct.
    - TODO: I should think about making a component called LoginForm to make my login page more modular.

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -3,14 +3,33 @@ import './styles.css';
 import SpanLink from '../../react-components/SpanLink';
 
 export default class Login extends React.Component {
-    render(){
+    render() {
         return (
             <div id="loginContainer">
                 <h1>This is the login page</h1>
-                <SpanLink to="/home" name="login"/>
-                <SpanLink to="/register" name="sign up"/>
-                <SpanLink to="/password reset" name="reset password"/>
-            </div>    
+                <br></br>
+
+                {/* A log in form */}
+                <div>
+                    <label>
+                        Username: <input type="text" name="username" />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Password: <input type="text" name="password" />
+                    </label>
+                </div>
+
+                <form action="/home">
+                    <br></br>
+                    <input type="submit" value="Log in" />
+                </form>
+                <br></br>
+
+                <SpanLink to="/register" name="Don't have an account? Sign up!"/>
+                <SpanLink to="/password reset" name="Forgot password?" />
+            </div>
         );
     }
 }
