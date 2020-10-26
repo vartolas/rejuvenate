5178c9fcdf03442a93c05d456aecaa9ac97a7af0
Danny <danisnotonfire.chen@mail.utoronto.ca>
2020-10-26 00:39:43 -0400

    Password reset page: I can prevent password resets with incorrect or weak credentials, and login with strong enough password.
    - TODO: Right now, a message appears below the password reset form indicating if the credentials are strong enough. If a password is weak, when I press enter or click on the "reset password" button, THEN it should display an error message.

--- src/views/Password Reset/index.js
+++ src/views/Password Reset/index.js
@@ -1,7 +1,56 @@
 import React from 'react';
 import './styles.css';
+import SpanLink from '../../react-components/SpanLink';
+
+const CORRECT_REGULAR_USER_USERNAME = "user";
+const CORRECT_REGULAR_USER_PASSWORD = "user";
 
 export default class PasswordReset extends React.Component {
+    // I found this helpful: https://reactjs.org/docs/forms.html
+    constructor(props) {
+        super(props);
+        this.state = {
+            newUsername: '',
+            newPassword: ''
+        };
+        // controlled components
+        this.updateUsername = this.updateUsername.bind(this);
+        this.updatePassword = this.updatePassword.bind(this);
+        this.tryToResetPassword = this.tryToResetPassword.bind(this);
+        this.displayError = this.displayError.bind(this);
+    }
+
+    updateUsername(e) {
+        e.preventDefault();
+        this.setState({ newUsername: e.target.value });
+    }
+
+    updatePassword(e) {
+        e.preventDefault();
+        this.setState({ newPassword: e.target.value });
+    }
+
+    tryToResetPassword() {
+        return this.state.newUsername !== '' &&
+            this.state.newPassword !== '' &&
+            this.state.newUsername === CORRECT_REGULAR_USER_USERNAME &&
+            this.state.newPassword === CORRECT_REGULAR_USER_PASSWORD;
+    }
+
+    displayError() {
+        if (this.state.newUsername === '') {
+            return <p>New username is missing.</p>;
+        } else if (this.state.newPassword === '') {
+            return <p>New password is missing.</p>;
+        } else if (this.state.newUsername !== CORRECT_REGULAR_USER_USERNAME) {
+            return <p>New username does not exist.</p>;
+        } else if (this.state.newPassword !== CORRECT_REGULAR_USER_PASSWORD) {
+            return <p>New password is not strong enough.</p>;
+        } else {
+            return <p>New username and password are strong enough.</p>;
+        }
+    }
+    
     render() {
         return (
             <div id="passwordResetContainer">
@@ -11,20 +60,23 @@ export default class PasswordReset extends React.Component {
                 {/* A password reset form */}
                 <div>
                     <label>
-                        Existing Username: <input type="text" name="username" />
+                        Existing Username: <input type="text" name="username"
+                            value={this.props.newUsername} onChange={this.updateUsername} />
                     </label>
                 </div>
                 <div>
                     <label>
-                        New Password: <input type="text" name="password" />
+                        New Password: <input type="text" name="password"
+                            value={this.props.newPassword} onChange={this.updatePassword}/>
                     </label>
                 </div>
 
+                { this.displayError() }
+
                 <form action="/">
-                    <br></br>
                     <input type="submit" value="Go Back" />
                 </form>
-                <form action="/home">
+                <form onSubmit={this.tryToResetPassword} action={this.tryToResetPassword() ? "/home" : "/password reset"}>
                     <br></br>
                     <input type="submit" value="Reset Password" />
                 </form>
