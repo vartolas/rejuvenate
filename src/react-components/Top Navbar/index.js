944f6377f77d6fccd998eeb171d38dd2f2ccd548
Danny <danisnotonfire.chen@mail.utoronto.ca>
2020-11-01 03:17:16 -0500

    I removed the extra indentation.

--- src/react-components/Top Navbar/index.js
+++ src/react-components/Top Navbar/index.js
@@ -8,26 +8,24 @@ export default class TopNavbar extends React.Component {
     render(){
         return (
             <div className="topRow">
-                    <BrowserRouter>
-                        <Navbar className="Navbar" bg="dark" variant="dark" expand="lg" sticky="top">
-                            <Navbar.Brand href="/home">
-                                    Fitness App
-                            </Navbar.Brand>
-                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                            <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="navContent">
-                                    <Nav.Link href="/profile">Profile</Nav.Link>
-                                    <Nav.Link href="/addFriend">Add Friend</Nav.Link>
-                                    <NavDropdown title="Stats" id="basic-nav-dropdown">
-                                        <NavDropdown.Item href="/recordStatistics">Record Stats</NavDropdown.Item>
-                                        <NavDropdown.Item href="/statistics">View Stats</NavDropdown.Item>
-                                    </NavDropdown>
-                                    <Nav.Link href="/setting">Setting</Nav.Link>
-                                    <Nav.Link href="/">Sign Out</Nav.Link>
-                            </Nav>
-                            </Navbar.Collapse>
-                        </Navbar>
-                    </BrowserRouter>
+                <BrowserRouter>
+                    <Navbar className="Navbar" bg="dark" variant="dark" expand="lg" sticky="top">
+                        <Navbar.Brand href="/home">Fitness App</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="navContent">
+                            <Nav.Link href="/profile">Profile</Nav.Link>
+                            <Nav.Link href="/addFriend">Add Friend</Nav.Link>
+                            <NavDropdown title="Stats" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="/recordStatistics">Record Stats</NavDropdown.Item>
+                                <NavDropdown.Item href="/statistics">View Stats</NavDropdown.Item>
+                            </NavDropdown>
+                            <Nav.Link href="/setting">Setting</Nav.Link>
+                            <Nav.Link href="/">Sign Out</Nav.Link>
+                        </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+                </BrowserRouter>
             </div>
         );
     }
