e6bd17d3e252a8f8136cc0359f58934f0379b43d
Danny <danisnotonfire.chen@mail.utoronto.ca>
2020-11-01 03:41:31 -0500

    I added a blank admin home page.

--- src/index.js
+++ src/index.js
@@ -12,9 +12,10 @@ import AdminRegister from './views/Admin Register';
 import AdminPasswordReset from './views/Admin Password Reset';
 
 import Home from './views/Home';
+import AdminHome from './views/Admin Home';
 import Profile from './views/Profile';
 import Statistics from './views/Statistics';
-import CreateStatistic from './views/Create Statistic';
+import CreateStatistics from './views/Create Statistic';
 import AddFriend from './views/Add Friend';
 import RecordStatistics from './views/Record Statistics';
 import Setting from './views/Setting';
@@ -27,34 +28,34 @@ var user = null;
 class App extends React.Component{
     render() {
         return (
-                <>
-                <TopNavbar/>
-                <BrowserRouter>
-                    <Switch>
-                        <Route exact path="/home" component={Home}/>
-                        
-                        <Route exact path="/profile" component={Profile} />
-                        <Route exact path="/password reset" component={PasswordReset}/>
-                        <Route exact path="/statistics" component={Statistics}/>
-                        <Route exact path="/statistics/create" component={CreateStatistic}/>
-                        <Route exact path="/addFriend" component={AddFriend}/>
-                        <Route exact path="/recordStatistics" component={RecordStatistics}/>
-                        <Route exact path="/setting" component={Setting}/>
-                    </Switch>
-                </BrowserRouter>
-                </>
+            <>
+            <TopNavbar/>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/home" component={Home}/>
+                    <Route exact path="/admin home" component={AdminHome} />
+                    <Route exact path="/profile" component={Profile} />
+                    <Route exact path="/password reset" component={PasswordReset}/>
+                    <Route exact path="/statistics" component={Statistics}/>
+                    <Route exact path="/statistics/create" component={CreateStatistics}/>
+                    <Route exact path="/addFriend" component={AddFriend}/>
+                    <Route exact path="/recordStatistics" component={RecordStatistics}/>
+                    <Route exact path="/setting" component={Setting}/>
+                </Switch>
+            </BrowserRouter>
+            </>
         );
     }
     
     componentDidMount() {
-        if(user == null){
+        if (user == null) {
             console.log("user not logged in");
         }
     }
 }
 
 class AppLoginWrapper extends React.Component {
-    render () {
+    render() {
         return (
             <BrowserRouter>
                 <Switch>
---
+++ src/views/Admin Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+export default class AdminHome extends React.Component {
+    render() {
+        return (
+            <div id="adminHomeContainer">
+                <h1>This is the admin home page.</h1>
+            </div>    
+        );
+    }
+}
