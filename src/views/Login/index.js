06b9f8be3ffdaf397e2741f5f2eceab6c128df76
Danny <danisnotonfire.chen@mail.utoronto.ca>
2020-11-05 15:19:31 -0500

    I wrapped all of my login form components in a single div.

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -8,22 +8,25 @@ export default class Login extends React.Component {
             <div id="loginContainer">
                 <h1>This is the login page</h1>
                 <br></br>
-                <UserLoginForm label1="Username" label2="Password"
-                    successfulLoginLink="/home" failureLoginLink="/"
-                    submitButtonName="Log in"
-                />
-                <br></br>
-                <form action="/register">
-                    <input type="submit" value="Sign Up" />
-                </form>
-                <br></br>
-                <form action="/password reset">
-                    <input type="submit" value="Reset Password" />
-                </form>
-                <br></br>
-                <form action="/admin">
-                    <input type="submit" value="Login in as Admin" />
-                </form>
+
+                <div id="loginComponent">
+                    <UserLoginForm label1="Username" label2="Password"
+                        successfulLoginLink="/home" failureLoginLink="/"
+                        submitButtonName="Log in"
+                    />
+                    <br></br>
+                    <form action="/register">
+                        <input type="submit" value="Sign Up" />
+                    </form>
+                    <br></br>
+                    <form action="/password reset">
+                        <input type="submit" value="Reset Password" />
+                    </form>
+                    <br></br>
+                    <form action="/admin">
+                        <input type="submit" value="Login in as Admin" />
+                    </form>
+                </div>
             </div>
         );
     }
