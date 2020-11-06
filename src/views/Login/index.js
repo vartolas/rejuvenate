8f9344a28e562c423d3bdbbff2b452b5a01379fc
Danny <danisnotonfire.chen@mail.utoronto.ca>
2020-11-05 19:59:52 -0500

    I removed the login page header.

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -6,9 +6,6 @@ export default class Login extends React.Component {
     render() {
         return (
             <div id="loginContainer">
-                <h1>This is the login page</h1>
-                <br></br>
-
                 <div id="loginComponent">
                     <UserLoginForm label1="Username" label2="Password"
                         successfulLoginLink="/home" failureLoginLink="/"
