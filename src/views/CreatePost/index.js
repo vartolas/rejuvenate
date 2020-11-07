3c9f4406096feb9f68f56bf2ecac414e8b9cb0d8
liumin53 <gavin.liu@mail.utoronto.ca>
2020-11-07 13:59:05 -0400

    finished styling create post page

--- src/views/CreatePost/index.js
+++ src/views/CreatePost/index.js
@@ -1,20 +1,41 @@
-import React from 'react';
+import React from "react";
+import { Form, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-import SpanLink from '../../react-components/SpanLink';
-
-import './styles.css';
+import "./styles.css";
 
 export default class CreatePost extends React.Component {
-    render (){
-        return (
-            <div id="createStatViewContainer">
-                <h1>Create New Post</h1>
-                    <form>
-                        <input name="Title" type="text"/>
-                    </form>
-                <SpanLink to="/home" name="return"/>
+	render() {
+		return (
+			<div className="createPostContainer">
+				<Form className="createPost">
+					<Form.Group controlId="Tag">
+						<Form.Label className="tagLabel">Tag</Form.Label>
+						<Form.Control as="select" className="tagSelect">
+							<option className="tagSelect">General</option>
+							<option className="tagSelect">Fitness</option>
+							<option className="tagSelect">Recipie</option>
+						</Form.Control>
+					</Form.Group>
 
-            </div>
-        );
-    }
+					<Form.Group controlId="Body">
+						<Form.Label className="contentLabel">Post Content</Form.Label>
+						<Form.Control as="textarea" rows={8} className="contentInput" />
+
+						<input type="file" name="file" id="file" class="inputPicture" />
+						<label for="file">Choose Picture</label>
+					</Form.Group>
+
+					<Button variant="primary" type="submit" className="postButton">
+						Post
+					</Button>
+					<Link to="/home">
+						<Button variant="primary" type="button" className="postButton">
+							Cancel
+						</Button>
+					</Link>
+				</Form>
+			</div>
+		);
+	}
 }
