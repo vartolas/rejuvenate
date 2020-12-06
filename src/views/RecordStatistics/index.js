7b47ff01c11a653b9981669ab83ba36618211986
Danny <danisnotonfire.chen@mail.utoronto.ca>
2020-12-05 23:23:05 -0500

    feat: I can click on the "Confirm all changes" button to go back to the view statistics page.

--- src/views/RecordStatistics/index.js
+++ src/views/RecordStatistics/index.js
@@ -10,31 +10,70 @@ export default class RecordStatistics extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			stat: null,
-			successfullyUpdated: null
+			stat: null
 		};
-		this.num_point_ids = 0;
-		this.statid = new URLSearchParams(window.location.search).get('id');
+		this.numPointIDs = 0;
+		this.statID = new URLSearchParams(window.location.search).get('id');
 	}
 
-	new_point_id = () => "point".concat(++this.num_point_ids);
+	getNewPointID = () => "point".concat(++this.numPointIDs);
 
-	componentDidMount(){
-		fetch(`${HOST_URL}/api/statistics/${this.statid}`)
+	componentDidMount() {
+		fetch(`${HOST_URL}/api/statistics/${this.statID}`)
 			.then(res => res.json())
-			.then(stat =>{
+			.then(stat => {
 				this.setState({stat: stat});
 			})
 	}
 
-	render() {
-		if(!this.state.stat){
-			return (
-				<LoadingDisplay/>
-			);
+	addPoint = (e) => {
+		e.preventDefault();
+		var x = parseInt(document.getElementById("newXEntryInput").value);
+		var y = parseFloat(document.getElementById("newYEntryInput").value);
+
+		if (this.state.stat.data.find((point) => point.x === x)) {
+			alert("This data point already exists.");
+			return;
 		}
 
-		this.num_point_ids = 0;
+		if (!isNaN(x) && !isNaN(y)) {
+			var newStat = this.state.stat;
+			newStat.data.push({ x: x, y: y });
+			newStat.data.sort((point1, point2) => point1.x - point2.x);
+			this.setState({ stat: newStat });
+		} else {
+			alert("This data point is invalid.");
+		}
+	};
+
+	removePoint = (index, e) => {
+		return function (e) {
+			let statToKeep = this.state.stat;
+			statToKeep.data.splice(index, 1);
+			this.setState({ stat: statToKeep });	
+		}
+	}
+
+	saveStatisticsChanges = (e) => {
+		fetch(`${HOST_URL}/api/statistics/${this.statID}`, {
+			method: 'PATCH',
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				data: this.state.stat.data
+			})
+		})
+		.then(res => res.json())
+		.then(updatedStat => {
+			this.setState({ stat: updatedStat });
+		});
+	};
+
+	render() {
+		if (!this.state.stat) {
+			return <LoadingDisplay/>;
+		}
+
+		this.numPointIDs = 0;
 		return (
 			<div id="recordStatViewContainer">
 				<h1>Record Stats</h1>
@@ -47,7 +86,7 @@ export default class RecordStatistics extends React.Component {
 								<tbody>
 									<tr>
 										<td>
-											<span id="xAxesLabel">{this.state.stat.xAxis}</span>{" "}
+											<span id="xAxesLabel">{this.state.stat.xAxis}</span>
 										</td>
 										<td>
 											<input id="newXEntryInput" type="text"></input>
@@ -63,8 +102,7 @@ export default class RecordStatistics extends React.Component {
 									</tr>
 								</tbody>
 							</table>
-
-							<Button id="newEntryButton" onClick={this.handleNewEntry}>
+							<Button id="newEntryButton" onClick={this.addPoint}>
 								Add New Entry
 							</Button>
 						</form>
@@ -73,8 +111,9 @@ export default class RecordStatistics extends React.Component {
 						<StatChart id="statChartDisplay" stat={this.state.stat} />
 					</div>
 				</div>
+
 				<div id="editEntriesSectionContainer">
-					<h2>Exisiting Entries</h2>
+					<h2>Existing Entries</h2>
 					<div id="editExistingStatContainer">
 						<div id="labelsContainer">
 							<span id="xAxesLabel">{this.state.stat.xAxis}</span>
@@ -82,19 +121,19 @@ export default class RecordStatistics extends React.Component {
 						</div>
 						<div id="editExistingStatScroll">
 							{this.state.stat.data.map((point, index) => {
-								var point_id = this.new_point_id();
+								var pointID = this.getNewPointID();
 								return (
 									<div
-										id={point_id}
-										key={point_id}
+										id={pointID}
+										key={pointID}
 										className="recordStatFormContainer"
 									>
 										<form className="pointForm">
 											<label>{point.x}</label>
 											<label>{point.y}</label>
 										</form>
-										<button onClick={this.deletePoint(index).bind(this)}>
-											-
+										<button onClick={this.removePoint(index)}>
+											x
 										</button>
 									</div>
 								);
@@ -103,55 +142,13 @@ export default class RecordStatistics extends React.Component {
 					</div>
 					<Button
 						id="confirmAllChangesButton"
-						onClick={this.handleConfirmAllChanges}
+						href="/statistics"
+						onClick={this.saveStatisticsChanges}
 					>
 						Confirm All Changes
 					</Button>
-					
 				</div>
 			</div>
 		);
-	}
-
-	handleConfirmAllChanges = (e) => {
-		fetch(`${HOST_URL}/api/statistics/${this.statid}`, {
-			method: 'PATCH',
-			headers: {"Content-Type": "application/json"},
-			body: JSON.stringify({
-				data: this.state.stat.data
-			})
-		})
-		.then(res => res.json())
-		.then(updatedstat => {
-			this.setState({stat: updatedstat})
-		});
-	};
-
-	handleNewEntry = (e) => {
-		e.preventDefault();
-		var x = parseInt(document.getElementById("newXEntryInput").value);
-		var y = parseFloat(document.getElementById("newYEntryInput").value);
-
-		if (this.state.stat.data.find((point) => point.x === x)) {
-			alert("value has already been entered");
-			return;
-		}
-
-		if (!isNaN(x) && !isNaN(y)) {
-			var new_stat = this.state.stat;
-			new_stat.data.push({ x: x, y: y });
-			new_stat.data.sort((point1, point2) => point1.x - point2.x);
-			this.setState({ stat: new_stat });
-		} else {
-			alert("invaid entries");
-		}
-	};
-
-	deletePoint(index, e) {
-		return function (e) {
-			var new_stat = this.state.stat;
-			new_stat.data.splice(index, 1);
-			this.setState({ stat: new_stat });
-		};
 	}
 }
